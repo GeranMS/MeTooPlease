337f7bfe0a3f1fd0bc1424b390f4cf788c07aa5f
GeranMS <geran.swarts@dyno-mo.com>
2023-07-21 08:19:11 +0200

    Added tooltips to coffee options and removed duplicates

--- client/src/components/Order.js
+++ client/src/components/Order.js
@@ -1,8 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import './Order.css';
-
 const SESSION_STORAGE_KEY = 'order_names';
-
+const coffeeOptions = [
+  { value: 'Usual', description: 'GOTO' },
+  { value: 'Cappucino', description: 'Espresso with steamed milk and a layer of foam' },
+  { value: 'Latte', description: '2x Espresso with a lot of steamed milk and a small amount of foam' },
+  { value: 'KG', description: 'Iykyk' },
+  { value: 'Cortado', description: '2x Espresso, steamed milk' },
+  { value: 'Flat White', description: '2x Espresso with steamed milk' },
+  { value: 'Americano', description: '2x Espresso with hawt water' },
+  { value: 'Macchiato', description: 'Espresso with foam' },
+  { value: 'Dirty Coffee', description: '2x Espresso poured over cold milk' },
+  { value: 'Mocha', description: '2x Espresso, chocolate and steamed milk' },
+  { value: 'Piccolo', description: 'Single cortado' },
+  { value: 'Espresso', description: 'Espresso' },
+  { value: 'Doppio', description: '2x Espresso' },
+  { value: 'Tea', description: 'T' },
+  { value: 'Red Cappucino', description: 'Red Espresso with steamed milk and a layer of foam' },
+  { value: 'Ice Coffee', description: 'All right stop; collaborate and listen...' },
+  { value: 'Hot Chocolate', description: 'Hot chockey powder with steamed milk' }
+];
 async function submitOrder(order) {
   const orderWithStatus = { ...order, status: "Pending" };
   const response = await fetch('https://hopaserver.azurewebsites.net/api/orders', {
@@ -18,7 +35,6 @@ async function submitOrder(order) {
   const data = await response.json();
   return data;
 }
-
 function Order() {
   const [name, setName] = useState('');
   const [coffeeType, setCoffeeType] = useState('');
@@ -26,7 +42,6 @@ function Order() {
   const [error, setError] = useState('');
   const [success, setSuccess] = useState('');
   const [cachedNames, setCachedNames] = useState([]);
-
   useEffect(() => {
     // Retrieve cached names from sessionStorage
     const cachedNamesJSON = sessionStorage.getItem(SESSION_STORAGE_KEY);
@@ -34,12 +49,10 @@ function Order() {
       setCachedNames(JSON.parse(cachedNamesJSON));
     }
   }, []);
-
   useEffect(() => {
     // Update cached names in sessionStorage
     sessionStorage.setItem(SESSION_STORAGE_KEY, JSON.stringify(cachedNames));
   }, [cachedNames]);
-
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (!name) {
@@ -49,7 +62,7 @@ function Order() {
     const order = { name, coffeeType, specialRequirements };
     try {
       const data = await submitOrder(order);
-      setSuccess('Order submitted successfully');      
+      setSuccess('Order submitted successfully');
       console.log('Order submitted:', data);
     } catch (error) {
       // setError('Failed to submit order');
@@ -63,123 +76,49 @@ function Order() {
       setCachedNames([...cachedNames, name]);
     }
   };
-
   const handleNameChange = (event) => {
     setName(event.target.value);
   };
-
   const handleCoffeeTypeChange = (event) => {
     setCoffeeType(event.target.value);
   };
-
   const handleSpecialRequirementsChange = (event) => {
     setSpecialRequirements(event.target.value);
   };
-
-//   return (
-//     <div className="order-container">
-//       <form className="order-form" onSubmit={handleSubmit} style={{ backgroundImage: `url('/images/coffePhoto.png')` }}>
-//         <label>
-//           Name:
-//           <input type="text" className="input-field" value={name} onChange={handleNameChange} />
-//         </label>
-//         <br />
-
-//         <label>
-//           Coffee Type:
-//           <select className="input-field" value={coffeeType} onChange={handleCoffeeTypeChange}>
-//             <option value="Usual">Usual</option>
-//             <option value="Cappucino">Cappucino</option>
-//             <option value="Latte">Latte</option>
-//             <option value="KG">KG</option>
-//             <option value="Cortado">Cortado</option>
-//             <option value="Flat White">Flat White</option>
-//             <option value="Americano">Americano</option>
-//             <option value="Macchiato">Flat White</option>
-//             <option value="Americano">Americano</option>
-//             <option value="Mocha">Americano</option>
-//             <option value="Piccolo">Piccolo</option>
-//             <option value="Espresso">Espresso</option>
-//             <option value="EspressoX2">Espresso</option>
-//             <option value="Tea">Tea</option>
-//             <option value="Red Cappucino">Red Cappucino</option>
-//             <option value="Ice Coffee">Ice Coffee</option>
-//           </select>
-//         </label>
-//         <br />
-
-//         <label>
-//           Special Requirements:
-//           <textarea className="input-field" value={specialRequirements} onChange={handleSpecialRequirementsChange} />
-//         </label>
-
-//         <br />
-//         <button type="submit" className="submit-button">Place Order</button>
-//       </form>
-
-//       {error && <p className="error-message">{error}</p>}
-//       {success && <p className="success-message">{success}</p>}
-
-//       <div className="cached-names-container">
-//         <h3>Cached Names:</h3>
-//         <ul>
-//           {cachedNames.map((cachedName) => (
-//             <li key={cachedName}>{cachedName}</li>
-//           ))}
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Order;
-
-return (
-  <div className="order-container">
-    <form className="order-form" onSubmit={handleSubmit} style={{ backgroundImage: `url('/images/coffePhoto.png')` }}>
-      <label>
-        Name:
-        <input type="text" className="input-field" value={name} onChange={handleNameChange} list="nameList" />
-        <datalist id="nameList">
-          {cachedNames.map((cachedName) => (
-            <option key={cachedName} value={cachedName} />
-          ))}
-        </datalist>
-      </label>
-      <br />
-      <label>
-        Coffee Type:
-        <select className="input-field" value={coffeeType} onChange={handleCoffeeTypeChange}>
-          <option value="Usual">Usual</option>
-          <option value="Cappucino">Cappucino</option>
-  //      <option value="Latte">Latte</option>
-  //      <option value="KG">KG</option>
-  //      <option value="Cortado">Cortado</option>
-  //      <option value="Flat White">Flat White</option>
-  //      <option value="Americano">Americano</option>
-  //      <option value="Macchiato">Flat White</option>
-  //      <option value="Americano">Americano</option>
-//        <option value="Mocha">Americano</option>
-//        <option value="Piccolo">Piccolo</option>
-//        <option value="Espresso">Espresso</option>
-//        <option value="EspressoX2">Espresso</option>
-//        <option value="Tea">Tea</option>
-//        <option value="Red Cappucino">Red Cappucino</option>
-//        <option value="Ice Coffee">Ice Coffee</option>
-        </select>
-      </label>
-      <br />
-      <label>
-        Special Requirements:
-        <textarea className="input-field" value={specialRequirements} onChange={handleSpecialRequirementsChange} />
-      </label>
-      <br />
-      <button type="submit" className="submit-button">Place Order</button>
-    </form>
-    {error && <p className="error-message">{error}</p>}
-    {success && <p className="success-message">{success}</p>}
-  </div>
-);
+  return (
+    <div className="order-container">
+      <form className="order-form" onSubmit={handleSubmit} style={{ backgroundImage: `url('/images/coffePhoto.png')` }}>
+        <label>
+          Name:
+          <input type="text" className="input-field" value={name} onChange={handleNameChange} list="nameList" />
+          <datalist id="nameList">
+            {cachedNames.map((cachedName) => (
+              <option key={cachedName} value={cachedName} />
+            ))}
+          </datalist>
+        </label>
+        <br />
+        <label>
+          Coffee Type:
+          <select className="input-field" value={coffeeType} onChange={handleCoffeeTypeChange}>
+            {coffeeOptions.map((option) => (
+              <option key={option.value} value={option.value} title={option.description}>
+                {option.value}
+              </option>
+            ))}
+          </select>
+        </label>
+        <br />
+        <label>
+          Special Requirements:
+          <textarea className="input-field" value={specialRequirements} onChange={handleSpecialRequirementsChange} />
+        </label>
+        <br />
+        <button type="submit" className="submit-button">Place Order</button>
+      </form>
+      {error && <p className="error-message">{error}</p>}
+      {success && <p className="success-message">{success}</p>}
+    </div>
+  );
 }
-
 export default Order;
